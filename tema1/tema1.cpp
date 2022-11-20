221bf99e6666bacbe565ae528070029352d6de16
CalinPavel <calinpav21@gmail.com>
2022-11-20 12:34:56 +0200

    finished

--- tema1/tema1.cpp
+++ tema1/tema1.cpp
@@ -27,14 +27,18 @@ void get_parameters(int argc, char **argv, int *M, int *R, vector<string> *files
 
 int binarySearch(vector<int> arr, int l, int r, int x)
 {
-	if (r >= l)
+	while (l <= r)
 	{
-		int mid = l + (r - l) / 2;
-		if (arr[mid] == x)
-			return mid;
-		if (arr[mid] > x)
-			return binarySearch(arr, l, mid - 1, x);
-		return binarySearch(arr, mid + 1, r, x);
+		int m = l + (r - l) / 2;
+
+		if (arr[m] == x)
+			return m;
+
+		if (arr[m] < x)
+			l = m + 1;
+
+		else
+			r = m - 1;
 	}
 	return -1;
 }
@@ -44,6 +48,8 @@ void *mapper_function(void *parameters)
 	struct mapper_memory *mapper = (struct mapper_memory *)parameters;
 
 	int i;
+next_file:
+	// extragere fisier din lista
 	pthread_mutex_lock(&lock);
 	std::vector<string> *file = mapper->files;
 	string file_to_check;
@@ -52,54 +58,84 @@ void *mapper_function(void *parameters)
 		file_to_check = (*file).back();
 		(*file).pop_back();
 	}
+	pthread_mutex_unlock(&lock);
 
 	string text;
 	ifstream MyReadFile(file_to_check);
-	// getline(MyReadFile, text);
 
 	std::vector<std::vector<int>> *check_memory = mapper->compare_memory;
 	std::vector<std::vector<int>> mem = mapper->mem;
 
 	vector<int> store;
 	store.push_back(-1);
-	for (i = 0; i <= mapper->R + 1; i++)
+	for (i = 0; i <= mapper->R + 2; i++)
 	{
 		mapper->mem.push_back(store);
 	}
-
+	getline(MyReadFile, text);
 	while (getline(MyReadFile, text))
 	{
-		for (i = 2; i <= mapper->R; i++)
+		for (i = 2; i <= mapper->R + 1; i++)
 		{
+			// se cauta in memoria precalculata
 			if (binarySearch((*check_memory)[i], 0, (*check_memory)[i].size() - 1, stoi(text)) != -1)
 			{
-				// store.push_back(stoi(text));
 				mapper->mem[i].push_back(stoi(text));
 			}
 		}
-		// cout << stoi(text) << "\n";
 	}
+	MyReadFile.close();
 
-	for (int i = 0; i < mapper->mem.size(); i++)
+	// se proceseaza urmatorul fisier din vector
+	if (!(*file).empty())
 	{
-		for (int j = 0; j < mapper->mem[i].size(); j++)
-		{
-			cout << mapper->mem[i][j] << " ";
-		}
-		cout << endl;
+		goto next_file;
 	}
 
-	cout << "///////////////////\n";
+	pthread_barrier_wait(&barrier);
+	pthread_exit(NULL);
+}
+
+void *reducer_function(void *parameters)
+{
+	// se verifica finalitatea thread-urilor de tip mapper
+	pthread_barrier_wait(&barrier);
+	pthread_mutex_lock(&lock);
+
+	int id, size;
+	vector<mapper_memory> *reducer_mem = (vector<mapper_memory> *)parameters;
+	mapper_memory reducer = (*reducer_mem)[0];
+	// asociere exponent 
+	id = reducer.reducer_id;
+	(*reducer_mem)[0].reducer_id++;
 	pthread_mutex_unlock(&lock);
 
-	// cout << (*check_memory)[1][1] << "\n";
+	size = reducer.size - 1;
 
-	// for(auto i = (*check_memory)[3].begin() ; i !=(*check_memory)[3].end() ; ++i){
-	// 	cout << *i << "\n";
-	// }
-	// cout << (*check_memory)[3].size()-1;
+	vector<int> data;
 
-	MyReadFile.close();
+	for (int i = 0; i <= size; i++)
+	{
+		mapper_memory reducer1 = (*reducer_mem)[i]; 
+		// parcurgerea vectorilor corespunzatori exponentului cautat
+		for (auto j = reducer1.mem[id].begin() + 1; j != reducer1.mem[id].end(); ++j)
+		{
+			// verificare unicitate numar
+			if (std::find(data.begin(), data.end(), *j) == data.end())
+			{
+				data.push_back(*j);
+			}
+		}
+	}
+	// scriere in fisier
+	string file = "out";
+	string to_add = std::to_string(id);
+	string fullname = file.append(to_add).append(".txt");
+
+	ofstream MyFile(fullname);
+	MyFile << data.size();
+	MyFile.close();
+
 	pthread_exit(NULL);
 }
 
@@ -113,83 +149,76 @@ int main(int argc, char **argv)
 	num_threads = M + R;
 
 	pthread_t threads[num_threads];
-	int arguments[num_threads];
+	pthread_barrier_init(&barrier, NULL, M + R);
 
-	pthread_barrier_init(&barrier, NULL, num_threads);
+	// precalculare puteri perfecte
 
-	///////////////////////////////////////////////////// build exp
 	std::vector<std::vector<int>> compare_memory;
-	int exponent, line = 0;
+	int exponent;
 	vector<int> store;
 
 	store.push_back(-1);
-	// line 0
 	compare_memory.push_back(store);
-	// line 1
 	compare_memory.push_back(store);
 	store.clear();
-	unsigned int value, count = 1;
+	unsigned long long value, count = 1;
 	for (exponent = 2; exponent <= R + 1; exponent++)
 	{
 	next:
 		value = count;
-		// cout << "Value:" << value << "\n";
 		for (i = 1; i <= exponent - 1; i++)
 		{
 			value *= count;
 		}
-		// cout << value << "\n";
 
-		if (value < 100)
+		if (value < INT32_MAX)
 		{
 			store.push_back((int)value);
 			value = count++;
-			// cout << "Count" << count << "\n";
 			goto next;
 		}
-		// cout << "/////////////////\n";
 		compare_memory.push_back(store);
 		count = 1;
 		store.clear();
 	}
-	/////////////////////////////////////////////////////build exp
 
 	vector<mapper_memory> mapper_mem;
 	for (i = 0; i < M; i++)
 	{
-		mapper_mem.push_back(mapper_memory());
+		mapper_mem.push_back(*(new mapper_memory()));
 		mapper_mem[i].files = &files;
 		mapper_mem[i].M = M;
 		mapper_mem[i].R = R;
 		mapper_mem[i].compare_memory = &compare_memory;
 	}
+	// primul exponent verificat
+	mapper_mem[0].reducer_id = 2;
+	// marimea vectorului de structuri
+	mapper_mem[0].size = mapper_mem.size();
 
-	// cout << files[1] << "\n";
-	// cout << mapper_mem[0].files;
-
-	for (int i = 0; i < M; i++)
+	for (int i = 0; i < M + R; i++)
 	{
-		r = pthread_create(&threads[i], NULL, mapper_function, (void *)&mapper_mem[i]);
-		if (r)
+		if (i < M)
 		{
-			printf("Eroare la crearea thread-ului %d\n", i);
-			exit(-1);
+			r = pthread_create(&threads[i], NULL, mapper_function, (void *)&mapper_mem[i]);
+			if (r)
+			{
+				printf("Eroare la crearea thread-ului %d\n", i);
+				exit(-1);
+			}
+		}
+		else
+		{
+			r = pthread_create(&threads[i], NULL, reducer_function, (void *)&mapper_mem);
+			if (r)
+			{
+				printf("Eroare la crearea thread-ului %d\n", i);
+				exit(-1);
+			}
 		}
 	}
 
-	// for (i = 0; i < R; i++)
-	// {
-	// 	arguments[i] = i;
-	// 	r = pthread_create(&threads[i], NULL, reducer, &arguments[i]);
-
-	// 	if (r)
-	// 	{
-	// 		printf("Eroare la crearea thread-ului %d\n", i);
-	// 		exit(-1);
-	// 	}
-	// }
-
-	for (i = 0; i < M; i++)
+	for (i = 0; i < M + R; i++)
 	{
 		r = pthread_join(threads[i], &status);
 
@@ -199,6 +228,7 @@ int main(int argc, char **argv)
 			exit(-1);
 		}
 	}
+
 	pthread_barrier_destroy(&barrier);
 	return 0;
 }
--- tema1/tema1.h
+++ tema1/tema1.h
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <vector>
+#include <algorithm>
 
 pthread_barrier_t barrier;
 
@@ -14,6 +15,8 @@ struct mapper_memory
     std::vector<std::vector<int>> *compare_memory;
     int M;
     int R;
+    int size;
+    int reducer_id;
 };
 
 void get_parameters(int argc, char **argv, int *M, int *R);
